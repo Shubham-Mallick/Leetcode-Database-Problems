47052f2a95eb9c6615987f0c12e72c9c3267dd00
Shubham Mallick <124443747+Shubham2937Q@users.noreply.github.com>
2025-07-02 19:19:28 +0530

    Time: 502 ms (66.53%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT p.project_id , ROUND(AVG(CAST(e.experience_years AS DECIMAL(10,2))), 2) as average_years
+FROM Project as p
+LEFT JOIN Employee as e 
+ON p.employee_id = e.employee_id
+GROUP by p.project_id ;
